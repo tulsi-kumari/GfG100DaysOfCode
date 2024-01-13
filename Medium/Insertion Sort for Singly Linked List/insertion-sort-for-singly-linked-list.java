2ca9e5598c408ea8f950014db49bba70568bff26
Tulsi Kumari <102869380+tulsi-kumari@users.noreply.github.com>
2024-01-13 12:41:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Insertion Sort for Singly Linked List/insertion-sort-for-singly-linked-list.java
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class insertion
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			insertion llist = new insertion(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.insertionSort(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    */
+class Solution
+{
+    public static Node insertionSort(Node head)
+    {
+        //code here
+        if(head == null || head.next == null)return head;
+        Node st=head;
+        Node temp=st.next;
+        while(temp!=null)
+        {
+            if(temp.data>=st.data)
+            {
+                st=temp;
+                temp=temp.next;
+            }else
+            {
+                st.next=temp.next;
+                Node curr=head;
+                Node prev=null;
+                while(curr!=st && curr.data<=temp.data)
+                {
+                    prev=curr;
+                    curr=curr.next;
+                }
+                if(prev==null)
+                {
+                    temp.next=head;
+                    head=temp;
+                }else
+                {
+                    temp.next=curr;
+                    prev.next=temp;
+                }
+                
+            }
+            temp=st.next;
+        }
+        return head;
+    }
+}
